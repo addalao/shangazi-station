8080290045590afd6f6d493a7456fbff30f622ec
addalao <1076435994@qq.com>
2022-06-10 11:53:21 +0800

    首页路由完成

---
+++ src/pages/ecology.vue
@@ -0,0 +1,3 @@
+<template>
+ecology
+</template>
--- src/route/index.js
+++ src/route/index.js
@@ -14,7 +14,41 @@ const router = createRouter({
             path: '/aboutUs',
             component: () => import('../pages/aboutUs.vue')
         },
-
+        {
+            name: 'News',
+            path: '/News',
+            component: () => import('../pages/news.vue')
+        },
+        {
+            name: 'productCenter',
+            path: '/productCenter',
+            component: () => import('../pages/productCenter.vue')
+        },
+        {
+            name: 'farmProgram',
+            path: '/farmProgram',
+            component: () => import('../pages/farmProgram.vue')
+        },
+        {
+            name: 'ecology',
+            path: '/ecology',
+            component: () => import('../pages/ecology.vue')
+        },
+        {
+            name: 'cloudPlatform',
+            path: '/cloudPlatform',
+            component: () => import('../pages/cloudPlatform.vue')
+        },
+        {
+            name: 'downloadCenter',
+            path: '/downloadCenter',
+            component: () => import('../pages/downloadCenter.vue')
+        },
+        {
+            name: 'cooperation',
+            path: '/cooperation',
+            component: () => import('../pages/cooperation.vue')
+        },
     ]
 })
 
